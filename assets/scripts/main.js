fe0a890876eac6dd24479b150e04ac156b2e6ba8
Louisa <ouisamus@gmail.com>
2024-02-01 16:24:18 -0500

    Added button functionality and RGB distance calculation

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,28 +2,73 @@
 const pickerContainer = document.getElementById('pickerContainer');
 const currentColorBox = document.getElementById('currentColorBox');
 const currentColorLabel = document.getElementById('currentColorLabel');
-const goalColorBox = document.getElementById('currentColorBox');
+const goalColorBox = document.getElementById('goalColorBox');
 const goalColorLabel = document.getElementById('goalColorLabel');
-goalColor = generateNewGoalColor();
+const nextButton = document.getElementById('nextButton');
+let goalColor;
 
 // Draw picker
-const picker = colorjoe.rgb(pickerContainer, '#ff0000', '');
+const picker = colorjoe.rgb(pickerContainer, 'red', '');
 picker.on("change", color => {
-    currentColorBox.style.backgroundColor = color.css()
-    let textColor = color.xyz()._y >= 0.5 ? 'black' : 'white';
-    currentColorLabel.style.color = textColor;
+    if (!nextButton.classList.contains('newGame')){
+        currentColorBox.style.backgroundColor = color.css()
+        currentColorLabel.style.color = color.xyz()._y >= 0.5 ? 'black' : 'white';
+    }
 });
 
-// Next button event listener
+// Reset
+resetGame();
 
-function generateNewGoalColor(){
+// Button event listener
+nextButton.addEventListener('click', buttonClick);
+
+function generateNewGoalColor() {
     const hexadecimalDigits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-    let random, color = "#";
-    for(i = 0; i < 6; i++){
+    let random, colorHex = "#";
+    for (i = 0; i < 6; i++) {
         random = Math.floor(Math.random() * 16);
-        color += hexadecimalDigits[random];
+        colorHex += hexadecimalDigits[random];
     }
-    goalColorLabel.innerText = color;
-    return color;
+    goalColorLabel.innerText = colorHex;
+    colorObj = one.color(colorHex);
+    return colorObj;
 }
 
+function buttonClick() {
+    nextButton.classList.toggle('newGame');
+    if (nextButton.classList.contains('newGame')) {
+        showResults();
+    } else {
+        resetGame();
+    }
+}
+
+function showResults() {
+    nextButton.innerText = 'New game';
+    console.log(computeRgbDistance(picker.get(), goalColor));
+    goalColorBox.style.backgroundColor = goalColor.hex().toUpperCase();
+    goalColorLabel.style.color = goalColor.xyz()._y >= 0.5 ? 'black' : 'white';
+    currentColorLabel.innerText = picker.get().hex().toUpperCase();
+}
+
+function resetGame() {
+    nextButton.innerText = 'Show score';
+    goalColor = generateNewGoalColor();
+    goalColorBox.style.backgroundColor = '#ddd';
+    goalColorLabel.style.color = 'black';
+    currentColorLabel.innerText = '??????';
+    picker.set('red');
+}
+
+function computeRgbDistance(c1, c2) {
+    let dr = c2.red() - c1.red();
+    let dg = c2.green() - c1.green();
+    let db = c2.blue() - c1.blue();
+    let distance = Math.sqrt(dr * dr + dg * dg + db * db);
+    distance /= Math.sqrt(3);
+    return Math.floor(distance * 1000) / 10;
+}
+
+function computeLabDistance(c1, c2) {
+
+}
