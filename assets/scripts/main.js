333985fe8381bbb98d6e1176888c14d3831e6cc6
Louisa <ouisamus@gmail.com>
2024-02-01 14:35:59 -0500

    added color boxes that change color

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,2 +1,12 @@
-const page = document.getElementById('page');
-const picker = colorjoe.hsl(page, '#113c38', '');
+const pickerContainer = document.getElementById('pickerContainer');
+
+// pick random color
+
+const picker = colorjoe.rgb(pickerContainer, '#f00', '');
+const currentColorBox = document.getElementById('currentColorBox');
+const currentColorLabel = document.getElementById('currentColorLabel');
+picker.on("change", color => {
+    currentColorBox.style.backgroundColor = color.css()
+    let textColor = color.xyz()._y >= 0.5 ? 'black' : 'white';
+    currentColorLabel.style.color = textColor;
+});
